669700ab9509e137b47e310b573565213cd27c84
NojusMat <d00249674@student.dkit.ie>
2024-11-24 21:05:06 +0000

    Initial Api Key Auth Commit

---
+++ Authentication/AuthConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace SOA_CA2_Cian_Nojus.Authentication
+{
+    public static class AuthConstants
+    {
+        public const string ApiKeySectionName = "Authentication:ApiKey";
+        public const string ApiKeyHeaderName = "X-Api-Key";
+    }
+}
